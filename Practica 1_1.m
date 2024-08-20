ce27928c285f1881d613ce1eb6f16d6b86513eb7
Roger Vasan <Rogelio>
2024-08-19 19:27:28 -0600

    Practica 1

---
+++ Practica 1_1.m
@@ -0,0 +1,22 @@
+disp("Practica1")
+theta1 = 0
+theta2 = 35
+d_thtat1 = 0
+d_theta2 = 0
+m1 = 0.2
+l1 = 0.3
+m2 = 0.1
+l2 = 0.25
+g = 9.81
+
+op1 = (m2*l2)/(m1 + m2)*l1
+op2 = (m1 + m2)/(m2*l2)
+op3 = cos(theta2 - theta1)
+op4 = d_theta2 * sin(theta2 - theta1)
+op5 = (g*sin(theta1))/l1
+
+
+op1_2 = 1/l2
+op2_2 = sin(theta2-theta1)
+op_3_2 = g*sin(theta2)
+op_4_2 = 1/l1
